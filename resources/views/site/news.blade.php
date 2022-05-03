f7e57d9b89ae3519a3f7904e129569c998a20ce1
is41-shakirovaash <shamilevna74@gmail.com>
2022-05-03 11:40:47 +0500

    Site 1.1

---
+++ resources/views/site/news.blade.php
@@ -0,0 +1,9 @@
+@extends('site.home')
+
+@section('content')
+    <img src="" alt="...">
+    <h3>Новость <span class="badge bg-secondary">New</span></h3>
+    <h5>Автор</h5>
+    <p>Текст</p>
+    <p>Дата</p>
+@endsection
